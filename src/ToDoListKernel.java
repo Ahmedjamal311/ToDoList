450dfb66789fc1a8aaa377bfe10a03e30b25d348
Ahmedjamal311 <ahmedjamal.jsr@gmail.com>
2025-03-07 22:10:11 -0500

    all work done

---
+++ src/ToDoListKernel.java
@@ -0,0 +1,50 @@
+import java.time.LocalDate;
+import java.util.Queue;
+
+import components.standard.Standard;
+
+/**
+ * @param description
+ *            the task that needs to be completed
+ * @param date
+ *            the date the task needs to be completed
+ */
+record Task(String description, LocalDate date) {
+}
+
+/**
+ *
+ */
+public interface ToDoListKernel extends Standard<Queue<Task>> {
+
+    /**
+     * Adds an array of two strings on being the task ad one being the due date.
+     *
+     * @param task
+     *            the string of the task that needs to be added
+     * @ensures this.length = this + task
+     * @updates this
+     */
+    void addTask(Task task);
+
+    /**
+     * removes the (task, due date) from the list from a certain index.
+     *
+     * @param index
+     *            the index
+     * @return a task object with task and date
+     * @requires index <= the length of this
+     * @ensures this = #this - task
+     * @updates this
+     */
+    Task removeTask(int index);
+
+    /**
+     * returns the length of the queue (@this) (number of tasks).
+     *
+     * @return length of the list (number of tasks)
+     * @ensures length >= 0
+     */
+    int length();
+
+}
