83a0b27cdb445e51993fdccf5f0018d4d1a1e13c
Ahmedjamal311 <ahmedjamal.jsr@gmail.com>
2025-04-10 10:21:55 -0400

    updated

--- src/ToDoList1.java
+++ src/ToDoList1.java
@@ -1,13 +1,12 @@
 import java.util.Comparator;
 
-import components.naturalnumber.NaturalNumberSecondary;
 import components.queue.Queue;
 import components.queue.Queue1L;
 
 /**
  *
  */
-public final class ToDoList1 extends NaturalNumberSecondary {
+public final class ToDoList1 extends ToDoListSecondary {
     /**
     *
     */
@@ -27,11 +26,12 @@ public final class ToDoList1 extends NaturalNumberSecondary {
      *            the string of the task that needs to be added
      * @updates this
      */
+    @Override
     public void addTask(Task task) {
         this.list.enqueue(task);
         this.list.sort(new Comparator<Task>() {
             @Override
-            public int compare(ToDoList1.Task o1, ToDoList1.Task o2) {
+            public int compare(Task o1, Task o2) {
                 return o1.date().compareTo(o2.date());
             }
         });
@@ -45,12 +45,13 @@ public final class ToDoList1 extends NaturalNumberSecondary {
      * @return a task object with task and date
      * @updates this
      */
+    @Override
     public Task removeTask(int index) {
         this.list.rotate(index);
         Task task = this.list.dequeue();
         this.list.sort(new Comparator<Task>() {
             @Override
-            public int compare(ToDoList1.Task o1, ToDoList1.Task o2) {
+            public int compare(Task o1, Task o2) {
                 return o1.date().compareTo(o2.date());
             }
         });
@@ -62,7 +63,48 @@ public final class ToDoList1 extends NaturalNumberSecondary {
      *
      * @return length of the list (number of tasks)
      */
+    @Override
     public int length() {
         return this.list.length();
+    }
+
+    /**
+     * transfers the information from copy to this and clears copy.
+     *
+     * @param copy
+     *            the list we want to copy from
+     */
+    @Override
+    public void transferFrom(Queue<Task> copy) {
+        this.list.clear();
+        while (copy.length() > 0) {
+            Task removed = copy.dequeue();
+            this.list.enqueue(removed);
+            this.list.sort(new Comparator<Task>() {
+                @Override
+                public int compare(Task o1, Task o2) {
+                    return o1.date().compareTo(o2.date());
+                }
+            });
+        }
+        copy.clear();
+    }
+
+    /**
+     * clears the information in this.
+     */
+    @Override
+    public void clear() {
+        while (this.list.length() > 0) {
+            this.list.dequeue();
+        }
+    }
+
+    /**
+     * creates a new instance of this.
+     */
+    @Override
+    public ToDoList1 newInstance() {
+        return new ToDoList1();
     }
 }
